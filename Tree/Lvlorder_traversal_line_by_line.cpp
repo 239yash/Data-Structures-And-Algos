40032886f16894062f4847daabf264bcea2b14de
yash gupta <53759576+239yash@users.noreply.github.com>
2020-04-26 17:45:02 +0530

    METHOD - 1

---
+++ Tree/Lvlorder_traversal_line_by_line.cpp
@@ -0,0 +1,58 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+// Function declaration line, just below this comment.
+struct node{
+    int key;
+    node * left;
+    node * right;
+    node(int x)
+    {
+        key = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void lvl_order_line(node * root)
+{
+    if( root == NULL){return;}
+
+    std::queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while( q.size() > 1)
+    {
+        node * curr = q.front();
+        q.pop();
+
+        if( curr == NULL)
+        {
+            cout<<"\n";
+            q.push(NULL);
+            continue;
+        }
+        cout<< curr -> key <<" ";
+        if( curr -> left != NULL)
+        q.push(curr -> left);
+
+        if( curr -> right != NULL)
+        q.push(curr -> right);
+    }
+}
+
+int main()
+{
+    node * root = NULL;
+    root = new node(10);
+    root -> left = new node(20);
+    root -> right = new node(30);
+    root -> left -> left = new node(40);
+    root -> left -> right = new node(50);
+    root -> right -> left = new node(60);
+    root -> right -> right = new node(70);
+
+    lvl_order_line(root);
+
+}
