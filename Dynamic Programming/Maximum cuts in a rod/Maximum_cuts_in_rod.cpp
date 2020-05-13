d627a09f218a704d424d3e86de840d9f8ad66fd7
yash gupta <53759576+239yash@users.noreply.github.com>
2020-05-13 11:47:25 +0530

    Create Maximum_cuts_in_rod.cpp

---
+++ Dynamic Programming/Maximum cuts in a rod/Maximum_cuts_in_rod.cpp
@@ -0,0 +1,40 @@
+ 
+# include<bits/stdc++.h>
+using namespace std;
+
+
+int maxcuts(int n, int a, int b, int c)
+{
+    int dp[n + 1];
+    for(int i = 1; i <= n; i++)
+    {
+        dp[i] = -1;
+    }
+    dp[0] = 0;
+    for(int i = 1; i <= n; i++)
+    {
+
+        if((i - a) >= 0)
+        { dp[i] = max(dp[i], dp[i-a]);}
+        
+        if((i - b) >= 0)
+        { dp[i] = max(dp[i], dp[i-b]);}
+
+        if((i - c) >= 0)
+        { dp[i] = max(dp[i], dp[i-c]);}
+
+        if(dp[i] != -1)
+        {
+            dp[i]++;
+        }
+
+    }
+
+
+        return dp[n];
+}
+
+int main()
+{
+    cout<<maxcuts(5,1,2,3);
+}
