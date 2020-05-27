469a340cd663472ba9ae966d4ca31d82b4ad3b57
yash gupta <53759576+239yash@users.noreply.github.com>
2020-05-27 12:42:49 +0530

    Create Binary_search_iterative.cpp

---
+++ Searching/Binary_search_iterative.cpp
@@ -0,0 +1,43 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[12] = {1, 2, 3, 4, 6, 8, 9, 11, 16, 18, 19, 25};
+
+int binary_search(int start, int end, int x)
+{
+    int mid;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (a[mid] == x)
+        {
+            return mid;
+        }
+        else
+        {
+            if (a[mid] <= x)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int result = binary_search(0, 11, 8);
+    if (result == -1)
+    {
+        cout << "the number not found.";
+    }
+    else
+    {
+        cout << "found at index " << result;
+    }
+    return 0;
+}
