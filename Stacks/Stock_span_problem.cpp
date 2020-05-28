a4afec0b6e490d2b6fe02764fd9347d41e1d50f4
yash gupta <53759576+239yash@users.noreply.github.com>
2020-05-28 15:59:01 +0530

    Create Stock_span_problem.cpp

---
+++ Stacks/Stock_span_problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void stock_span(int arr[], int n)
+{
+    std::stack<int> s;
+    s.push(0);
+    cout << "1"
+         << " ";
+
+    for (int i = 1; i < n; i++)
+    {
+        while (s.empty() == false && arr[s.top()] <= arr[i])
+        {
+            s.pop();
+        }
+
+        int span = (s.empty()) ? i + 1 : (i - s.top());
+        cout << span << " ";
+        s.push(i);
+    }
+}
+
+int main()
+{
+    int arr[] = {15, 13, 12, 14, 16, 8, 6, 4, 10, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    stock_span(arr, n);
+}
