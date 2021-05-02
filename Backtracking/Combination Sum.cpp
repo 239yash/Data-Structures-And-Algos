45c875fd893297936a5cda293a4601acae1d6d58
Yash Gupta <53759576+239yash@users.noreply.github.com>
2021-05-02 22:32:31 +0530

    Update Combination Sum.cpp

--- Backtracking/Combination Sum.cpp
+++ Backtracking/Combination Sum.cpp
@@ -39,3 +39,14 @@ public:
         func(v, can, target, v1, i+1);
     }
 };
+
+
+/*
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+*/
