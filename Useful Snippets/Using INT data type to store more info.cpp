3d511382f24195a7ce023fe295061889eccf3ce0
Yash Gupta <53759576+239yash@users.noreply.github.com>
2021-05-07 10:10:35 +0530

    Create Using INT data type to store more info.cpp

---
+++ Useful Snippets/Using INT data type to store more info.cpp
@@ -0,0 +1,8 @@
+/*
+
+Let's say, you are having a problem where you have to operate on binary numbers 0/1 only.
+Now, problem asked you to store some more information or lets say you have to store some extra info. 
+without changing the binary numbers. ints but only the 1-bit is used, I use the 2-bit to store the new state.
+At the end, replace the old state with the new state by shifting all values one bit to the right.
+
+*/
