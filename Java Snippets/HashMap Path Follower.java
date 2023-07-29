b18fb9305e0d8c25b6f7b7948043e10bbc9b680e
Yash Gupta <53759576+239yash@users.noreply.github.com>
2023-07-29 17:14:00 +0530

    Create HashMap Path Follower.java

---
+++ Java Snippets/HashMap Path Follower.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+        int c = 12;
+        HashMap b = new HashMap();
+        b.put("c", c);
+        HashMap a = new HashMap();
+        a.put("b", b);
+        HashMap obj = new HashMap();
+        obj.put("a", a);
+
+        // System.out.println(obj);
+        // obj = {
+        //     a: {
+        //         b: {
+        //             c: 12
+        //         }
+        //     }
+        // };
+
+
+        System.out.println(findPath(obj, "a:b:c")); // 12
+        System.out.println(findPath(obj, "a:b")); // {c: 12}
+        System.out.println(findPath(obj, "a")); // null
+        System.out.println(findPath(obj, "a:c")); // null
+        System.out.println(findPath(obj, "a:b:c:d")); // null
+        System.out.println(findPath(obj, "a:b:c:d:e")); // null
+  }
+  
+  public static Object findPath(HashMap obj, String path) {
+      String[] keyArr = path.split(":");
+      Object result = obj;
+
+      for(String key: keyArr) {
+          if (obj.containsKey(key)) {
+            if (obj.get(key) instanceof HashMap) {
+              obj = (HashMap) obj.get(key);
+              result = obj;
+            } else {
+              result = obj.get(key);
+            }
+          } else {
+              result = null;
+              break;
+          }
+      }
+
+      return result;
+  }
+}
