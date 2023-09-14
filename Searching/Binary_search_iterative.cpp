afbcb40c98e8e28c00bf31a1c8064ac793c5be2f
Yash Gupta <53759576+239yash@users.noreply.github.com>
2023-09-14 08:12:22 +0530

    Update Binary_search_iterative.cpp

--- Searching/Binary_search_iterative.cpp
+++ Searching/Binary_search_iterative.cpp
@@ -1,43 +1,14 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int a[12] = {1, 2, 3, 4, 6, 8, 9, 11, 16, 18, 19, 25};
-
-int binary_search(int start, int end, int x)
-{
-    int mid;
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-        if (a[mid] == x)
-        {
-            return mid;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid - 1;
+            else start = mid + 1;
         }
-        else
-        {
-            if (a[mid] <= x)
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                end = mid - 1;
-            }
-        }
+        return -1;
     }
-}
-
-int main()
-{
-    int result = binary_search(0, 11, 8);
-    if (result == -1)
-    {
-        cout << "the number not found.";
-    }
-    else
-    {
-        cout << "found at index " << result;
-    }
-    return 0;
-}
+};
