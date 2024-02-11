9b2f06655ecbf34473d3d068eae3c23608df77d1
Yash Gupta <53759576+239yash@users.noreply.github.com>
2024-02-11 23:32:53 +0530

    update: trie insert, search & prefix match

--- Trie/Trie_insertion_searching.cpp
+++ Trie/Trie_insertion_searching.cpp
@@ -1,86 +1,60 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-
-struct trienode {
-    struct trienode * child[26];
-    bool isEnd;
-
-    trienode()
-    {
-        isEnd = false;
-        for(int i = 0; i < 26; i++)
-        {
-            child[i] = NULL;
+class TrieNode {
+    public:
+        TrieNode* children[26];
+        bool flag;
+        TrieNode() {
+            flag = false;
+            for (int i = 0; i < 26; i++)
+                children[i] = NULL;
         }
-    }
 };
 
-class trie
-{
+
+class Trie {
 public:
-    trie()
-    {
-        root = new trienode();
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
     }
-    void insert_str(string &s, int n);
-    bool search_str(string &s, int n);
-private:
-    trienode* root;
+    
+    void insert(string word) {
+        TrieNode* node = root;
+		for (int i = 0; i < word.size(); i++) {
+			if (node -> children[word[i] - 'a'] == NULL) {
+				node -> children[word[i] - 'a'] = new TrieNode();
+			}
+			node = node -> children[word[i] - 'a'];
+		}
+        node -> flag = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        for (int i = 0; i < word.size(); i++) {
+            char c = word[i];
+            if (node -> children[c - 'a'] == NULL)
+                return false;
+            node = node -> children[c - 'a'];
+        }
+        return node -> flag;
+    }
+    
+    bool startsWith(string word) {
+        TrieNode* node = root;
+        for (int i = 0; i < word.size(); i++) {
+            char c = word[i];
+            if (node -> children[c - 'a'] == NULL)
+                return false;
+            node = node -> children[c - 'a'];
+        }   
+        return true;
+    }
 };
 
-
-
-void trie::insert_str(string &s, int n)
-{
-    trienode * curr = root;
-    int i;
-    for(i = 0; i < n; i++)
-    {
-        int index = s[i] - 'a';
-        if(curr -> child[index] == NULL)
-        {
-            curr -> child[index] = new trienode();
-        }
-        else
-        {
-            curr = curr -> child[index];
-        }
-    }
-
-    curr -> isEnd = true;
-}
-
-bool trie::search_str(string &s, int n)
-{
-    trienode * curr = root;
-    for(int i = 0; i < n; i++)
-    {
-        int index = s[i] - 'a';
-        if(curr -> child[index] != NULL)
-        {
-            curr = curr -> child[index];
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    if( curr -> isEnd == true)
-    return true;
-    else
-    {
-        return false;
-    }
-
-}
-
-int main()
-{
-    trie t;
-    string s1 = "yash";
-    t.insert_str(s1, 4);
-    cout<<t.search_str(s1,4);
-}
-
-
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
