2bb68b435f1c7cf0f7deb3cf0af4eece79d2691f
Yash Gupta <53759576+239yash@users.noreply.github.com>
2024-02-19 07:46:15 +0530

    Create All Subsets.cpp

---
+++ Backtracking/All Subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    std::vector<vector<int>> ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        std::vector<int> temp;
+        int index = 0;
+        func(nums, index, temp);
+        return ans;
+    }
+    
+    void func(vector<int>& nums, int index, vector<int> temp) {
+        if (index >= nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        
+        func(nums, index + 1, temp);
+        temp.push_back(nums[index]);
+        func(nums, index + 1, temp);
+        temp.pop_back();
+    }
+};
