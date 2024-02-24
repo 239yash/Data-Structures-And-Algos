42cf9ee4a6b58cdba1cbf1bc1715e29ab68a7a8b
Yash Gupta <53759576+239yash@users.noreply.github.com>
2024-02-25 00:53:22 +0530

    Update Union By Rank.cpp

--- Disjoint Set/Union By Rank.cpp
+++ Disjoint Set/Union By Rank.cpp
@@ -1,96 +1,35 @@
-#include <bits/stdc++.h> 
-using namespace std;
+class UnionFind {
+    public:
+    std::vector<int> representative;
+    std::vector<int> rank;
 
-//Function declarations.
-int find_parent(int);
-
-// Declaring the array size and declaring parent array of n size.
-const int n = 10;
-int parent[n];
-int rank_arr[n];
-
-// Function to initialize the parent array.
-// Initializing same value as their index because they are their's own parents.
-void initialize()
-{
-    for(int i = 0; i < n; i++)
-    {
-        parent[i] = i;
-        rank_arr[i] = 0;
-    }
-}
-
-// Function to find the representative of a element inthe parent array.
-int find_parent(int x)
-{
-    // If the element's parent is itself only, then simply return.
-    // Else, recursively find for the parent of it.
-    if(parent[x] == x)
-    {
-        return x;
-    }
-    else
-    {
-        return(find_parent(parent[x]));
-    }
-    
-}
-
-
-// Function to do the union of two disjoint sets.
-void union_set(int x, int y)
-{
-    int x_rep = find_parent(x);
-    int y_rep = find_parent(y);
-
-    // If the representatives of both are same, then do nothing.
-    // Else change the representative of one to another.
-    if(x_rep == y_rep)
-    {
-        return;
+    UnionFind(int n) {
+        for (int i = 0; i < n; i++) {
+            representative.push_back(i);
+            rank.push_back(0);
+        }
     }
 
-    // The tree with smaller rank will be made a child of the tree with larger rank.
-    if(rank_arr[x_rep] < rank_arr[y_rep])
-    {
-        parent[x_rep] = y_rep;
-
-    }
-    // If rank of x_rep is greater than y_rep.
-    else if(rank_arr[y_rep] < rank_arr[x_rep])
-    {
-        parent[y_rep] = x_rep;
-
-    }
-    // If the ranks are same.
-    else
-    {
-        parent[y_rep] = x_rep;
-        rank_arr[x_rep]++;
-    }
-    
-}
-
-int main()
-{
-    initialize();
-    union_set(3,4);
-    union_set(3,2);
-    union_set(6,7);
-    union_set(7,8);
-    union_set(8,9);
-    union_set(3,6);
-
-    for(int i = 0; i < n; i++)
-    {
-        cout<<parent[i]<<" ";
+    int find(int u) {
+        if (representative[u] == u) 
+            return u;
+        return representative[u] = find(representative[u]);
     }
 
-    cout<<"\n";
-
-    for(int i = 0; i < n; i++)
-    {
-        cout<<rank_arr[i]<<" ";
+    void combine(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+        if (rootU == rootV) {
+            return;
+        }
+        
+        if (rank[rootU] > rank[rootV]) {
+            representative[rootV] = rootU;
+        } else if (rank[rootU] < rank[rootV]) {
+            representative[rootU] = rootV;
+        } else {
+            representative[u] = v;
+            rank[v]++;
+        }
     }
-
-}
+};
