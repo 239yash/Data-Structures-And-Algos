aaa59f0265fd13ec1b55f20bbe976984090ba352
Yash Gupta <53759576+239yash@users.noreply.github.com>
2024-08-02 10:30:08 +0530

    Create Minimum Swaps to Group All 1's Together II.cpp

---
+++ Arrays/Minimum Swaps to Group All 1's Together II.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int totalOnes = 0, n = nums.size(), minZeroes = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            nums.push_back(nums[i]);
+            if (nums[i] == 1) {
+                totalOnes++;
+            }
+        }
+        if (totalOnes <= 1) return 0;
+        
+        n = n + totalOnes - 2;
+        int currZeroes = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            int curr = nums[i];
+            if (i < totalOnes - 1) {
+                if (curr == 0) currZeroes++;
+            } else if (i == totalOnes - 1) {
+                if (curr == 0) currZeroes++;
+                minZeroes = min(minZeroes, currZeroes);
+                // cout<<i<<" "<<currZeroes<<endl;
+            } else {
+                if (curr == 0) currZeroes++;
+                if (nums[i - totalOnes] == 0) currZeroes--;
+                minZeroes = min(minZeroes, currZeroes);
+                // cout<<i<<" "<<currZeroes<<endl;
+            }
+        }
+        return minZeroes;
+    }
+};
+
+
+
+
+
+
+// A swap is defined as taking two distinct positions in an array and swapping the values in them.
+
+// A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+
+// Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,1,1,0,0]
+// Output: 1
+// Explanation: Here are a few of the ways to group all the 1's together:
+// [0,0,1,1,1,0,0] using 1 swap.
+// [0,1,1,1,0,0,0] using 1 swap.
+// [1,1,0,0,0,0,1] using 2 swaps (using the circular property of the array).
+// There is no way to group all 1's together with 0 swaps.
+// Thus, the minimum number of swaps required is 1.
+// Example 2:
+
+// Input: nums = [0,1,1,1,0,0,1,1,0]
+// Output: 2
+// Explanation: Here are a few of the ways to group all the 1's together:
+// [1,1,1,0,0,0,0,1,1] using 2 swaps (using the circular property of the array).
+// [1,1,1,1,1,0,0,0,0] using 2 swaps.
+// There is no way to group all 1's together with 0 or 1 swaps.
+// Thus, the minimum number of swaps required is 2.
+// Example 3:
+
+// Input: nums = [1,1,0,0,1]
+// Output: 0
+// Explanation: All the 1's are already grouped together due to the circular property of the array.
+// Thus, the minimum number of swaps required is 0.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
